ee0b405e808d177db33f87ebf416f0b4a82eb7d6
Boldeusz <mnpower@poczta.onet.pl>
2021-01-20 13:12:06 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(NumberOfDaysInMonth.isLeapYear(2000));
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(2,2020));
+    }
+}
